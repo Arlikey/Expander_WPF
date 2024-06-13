c52667a7c249be0973b795a43e6a488e302bfbfb
admin <admin@DESKTOP-FA5KDQG>
2024-06-13 14:15:52 +0300

    Add project files.

---
+++ AdditionalTask1.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Expander_WPF
+{
+    /// <summary>
+    /// Interaction logic for AdditionalTask1.xaml
+    /// </summary>
+    public partial class AdditionalTask1 : Window
+    {
+        private TaskViewModel viewModel;
+        public AdditionalTask1()
+        {
+            InitializeComponent();
+            viewModel = new TaskViewModel();
+            DataContext = viewModel;
+            
+        }
+
+        private void addTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(ValidateFields())
+            {
+                Task task = new Task() { Title = nameTextBox.Text, Description = descriptionTextBox.Text };
+                viewModel.Tasks.Add(task);
+                ClearFields();
+            }
+         
+        }
+
+        private bool ValidateFields()
+        {
+            if (string.IsNullOrEmpty(nameTextBox.Text))
+            {
+                MessageBox.Show("Не было введено название задачи!", "Проверка ввода");
+                return false;
+            }
+            if (string.IsNullOrEmpty(descriptionTextBox.Text))
+            {
+                MessageBox.Show("Не было введено описание задачи!", "Проверка ввода");
+                return false;
+            }
+            return true;
+        }
+
+        private void ClearFields()
+        {
+            nameTextBox.Text = string.Empty;
+            descriptionTextBox.Text = string.Empty;
+        }
+    }
+
+    public class TaskViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Task> tasks;
+
+        public ObservableCollection<Task> Tasks
+        {
+            get { return tasks; }
+            set
+            {
+                tasks = value;
+                OnPropertyChanged("Tasks");
+            }
+        }
+
+        public TaskViewModel()
+        {
+            Tasks = new ObservableCollection<Task>
+            {
+                new Task { Title = "Задача №1", Description = "Описание задачи №1", Priority = Priority.Low },
+                new Task { Title = "Задача №2", Description = "Описание задачи №2", Priority = Priority.High },
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expander_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public BindingList<Product> Products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            LoadPreviewData();
+        }
+
+        private void LoadPreviewData()
+        {
+            Products = new BindingList<Product>()
+            {
+                new Product("Смарт-часы \"TechTime Pro\"", "Эти смарт-часы оснащены AMOLED-дисплеем, отслеживанием сердечного ритма, GPS и множеством фитнес-функций. " +
+                "Водонепроницаемые до 50 метров, с поддержкой уведомлений и до 10 дней автономной работы.", 199.99m, DateTime.Now),
+                new Product("Рюкзак \"Urban Explorer\"", "Стильный и функциональный рюкзак для городских жителей. Вмещает ноутбук до 15 дюймов, имеет много карманов для аксессуаров и защищен от воды. " +
+                "Идеально подходит для учебы и работы.", 59.99m, DateTime.Now)
+            };
+            foreach (Product p in Products)
+            {
+                productsListBox.Items.Add(CreateProductExpander(p));
+            }
+        }
+        private Expander CreateProductExpander(Product product)
+        {
+            Expander expander = new Expander()
+            {
+                Header = product.Name
+            };
+
+            StackPanel stackPanel = new StackPanel();
+            TextBlock priceTextBlock = new TextBlock()
+            {
+                Text = $"Цена : {product.Price}$",
+                FontWeight = FontWeights.Bold
+            };
+            TextBlock descriptionTextBlock = new TextBlock()
+            {
+                Text = $"\nОписание : {product.Description}",
+                FontWeight = FontWeights.Bold
+            };
+
+            TextBlock addDateTextBlock = new TextBlock()
+            {
+                Text = $"\nВремя добавления : {product.AddDate}",
+                FontStyle = FontStyles.Italic
+            };
+            expander.Tag = product;
+            stackPanel.Children.Add(priceTextBlock);
+            stackPanel.Children.Add(descriptionTextBlock);
+            stackPanel.Children.Add(addDateTextBlock);
+
+            expander.Content = stackPanel;
+            return expander;
+        }
+        private void RefreshExpander()
+        {
+            productsListBox.Items.Clear();
+            foreach (Product p in Products)
+            {
+                productsListBox.Items.Add(CreateProductExpander(p));
+            }
+        }
+        private void addProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddProductForm addProductForm = new AddProductForm();
+            addProductForm.AddProduct += AddProductForm_AddProduct;
+            addProductForm.ShowDialog();
+        }
+
+        private void AddProductForm_AddProduct(Product obj)
+        {
+            Products.Add(obj);
+            RefreshExpander();
+        }
+
+        private void deleteProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(productsListBox.SelectedItem != null)
+            {
+                Products.Remove((productsListBox.SelectedItem as Expander).Tag as Product);
+                RefreshExpander();
+            }           
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+
+    public class Product
+    {
+        public Product(string name, string description, decimal price, DateTime addTime)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            AddDate = addTime;
+        }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime AddDate { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
